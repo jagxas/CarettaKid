4c9b83f7d6e6ff2967ba9848fcc90f144f56e1ae
erenoku <erenoku910@gmail.com>
2022-06-06 17:24:25 +0300

    add ability to use different cruise speeds for right and left motors

--- motors.cpp
+++ motors.cpp
@@ -1,7 +1,6 @@
 #include "motors.hpp"
 #include "constants.hpp"
 Motors::Motors(int M1F, int M1B, int M2F, int M2B, int M1S, int M2S) : pin_m1f(M1F), pin_m1b(M1B), pin_m2f(M2F), pin_m2b(M2B), pin_m1s(M1S), pin_m2s(M2S) {
-	
 }
 
 void Motors::setup() {
@@ -10,7 +9,7 @@ void Motors::setup() {
 
 	pinMode(pin_m2f, OUTPUT);
 	pinMode(pin_m2b, OUTPUT);
-	
+
 	pinMode(pin_m1s, OUTPUT);
 	pinMode(pin_m2s, OUTPUT);
 }
@@ -20,9 +19,9 @@ void Motors::go(){
 	digitalWrite(pin_m1b, LOW);
 	digitalWrite(pin_m1f, HIGH);
 	digitalWrite(pin_m2b, LOW);
-	
-	digitalWrite(pin_m1s, CRUISE_SPEED);
-	digitalWrite(pin_m2s, CRUISE_SPEED);	
+
+	analogWrite(pin_m1s, CRUISE_SPEED_M1);
+	analogWrite(pin_m2s, CRUISE_SPEED_M2);
 }
 
 void Motors::turn_right(){
@@ -30,9 +29,9 @@ void Motors::turn_right(){
 	digitalWrite(pin_m1b, LOW);
 	digitalWrite(pin_m2f, LOW);
 	digitalWrite(pin_m2b, LOW);
-	
-	digitalWrite(pin_m1s, TURNING_SPEED);
-	delay(500); 	
+
+	analogWrite(pin_m1s, TURNING_SPEED);
+	delay(500);
 }
 
 void Motors::step_back(){
@@ -40,8 +39,8 @@ void Motors::step_back(){
 	digitalWrite(pin_m1b, HIGH);
 	digitalWrite(pin_m2f, LOW);
 	digitalWrite(pin_m2b, HIGH);
-	
-	digitalWrite(pin_m1s, CRUISE_SPEED);
-	digitalWrite(pin_m2s, CRUISE_SPEED);	
+
+	analogWrite(pin_m1s, CRUISE_SPEED_M1);
+	analogWrite(pin_m2s, CRUISE_SPEED_M2);
 	delay(500);
 }
